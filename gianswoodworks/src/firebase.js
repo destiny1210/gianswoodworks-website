a851edd403d3de10bcff474f71a6c18df976970e
destiny1210 <93067433+destiny1210@users.noreply.github.com>
2023-11-05 19:43:43 -0600

    added fire base db config

---
+++ gianswoodworks/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCL3HTFsdw4U04K1dPyk3N2Q31t8vD2SD0",
+    authDomain: "gian-s-woodworks.firebaseapp.com",
+    projectId: "gian-s-woodworks",
+    storageBucket: "gian-s-woodworks.appspot.com",
+    messagingSenderId: "552379451426",
+    appId: "1:552379451426:web:3c8220e23b005487f2e27b",
+    measurementId: "G-93XBXRM80X"
+  };
